0db395780ea5c709faf9e7626d32eaab5d244e59
Admin012 <roma.prihodko.1997@mail.ru>
2017-06-25 23:12:27 +0300

    Update Безымянный3.cpp

--- Program/Безымянный3.cpp
+++ Program/Безымянный3.cpp
@@ -9,11 +9,11 @@ int main()
 { 	
 	float x,y,G;
 	setlocale(LC_CTYPE, "ukr");
-	cout<<"авпвп x: ";
+	cout<<"vvedit x: ";
 	cin>>x;
-	cout<<"вапвап y: ";
+	cout<<"vuvedit y: ";
 	cin>>y;
 	G=(x+sin(y+5.2)*log(abs(x*y)))/(sqrt(x*x+pow(y,4))-cos((x*y)/(sqrt(x*x+y*y))));
-	cout<<"вапвапва: "<<G;
+	cout<<"vuvedit: "<<G;
 	getch();
 }
